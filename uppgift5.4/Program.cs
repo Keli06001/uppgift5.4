2411ceb0a0f3f7e437551794761fc0a223feb37a
Keli06001 <keli06001@utb.vaxjo.se>
2023-11-30 20:23:42 +0100

    Add project files.

---
+++ uppgift5.4/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace uppgift5_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] namnArray = { "Kevin", "Bert", "Stina", "Alfred", "Moa" };
+            Array.Sort(namnArray);
+            foreach(string namn in namnArray)
+            {
+                Console.WriteLine(namn);
+            }
+        }
+    }
+}
